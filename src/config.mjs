ddc93a603f9b1882b107d334d7f7c88fba898b58
Gilles Kagarama <gilles.kagarama@Gilless-MacBook-Pro.local>
2019-04-14 21:45:47 +0200

    Configure host

--- src/config.mjs
+++ src/config.mjs
@@ -1,5 +1,5 @@
 let config = {};
 const location = window.location;
 config.url = 'http://localhost:3000/api/v2/';
-if(location.host != '127.0.0.1:5500') config.url = location.host;
+if(location.host != '127.0.0.1:5500') config.url = "https://epicmailbox.herokuapp.com/api/v2/";
 export default config;
